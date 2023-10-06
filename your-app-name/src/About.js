eddcfffb5cfc668a24e9ab2f09c6a36df6aab322
james collier <astonjames@iahni.com>
2023-10-06 15:34:05 -0600

    playing around with template and postman

---
+++ your-app-name/src/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+    return (
+        <div id="about">
+            <h2>About Me</h2>
+            <p>I made this</p>
+            <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+            <div>
+                <h3>Links</h3>
+                <a href="https://github.com/iahni">GitHub</a>
+                <a href="https://www.linkedin.com/in/liza/">Oh yea</a>
+            </div>
+        </div>
+    );
+}
+
+export default About;
