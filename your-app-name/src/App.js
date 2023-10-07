f337c25bebfb4f90b17e59ca687890d6e851fe04
james collier <astonjames@iahni.com>
2023-10-06 18:31:49 -0600

    adding commit

--- your-app-name/src/App.js
+++ your-app-name/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Switch, Route} from ' react-router-dom';
 import Home from './Home';
 import Chat from './Chat';
 
 function App(){
   return (
     <Router>
-      <Route exact path ="/" component
-      ={Home}/>
-      <Route path ="/chat" component={Chat}/>
-
+      <div>
+        <h1>
+          Messages App
+        </h1>
+        <Switch>
+          <Route exact path = "/" component = {Home}/>
+          <Route path = "/chat"
+          component = {Chat}/>
+        </Switch>
+      </div>
     </Router>
   );
 }
+
+export default App;
+
 
