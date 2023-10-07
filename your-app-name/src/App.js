27ec0aa62e229878244ec24d5f84539a0e30aeaa
james collier <astonjames@iahni.com>
2023-10-07 11:32:17 -0600

    added more babel dependencies

--- your-app-name/src/App.js
+++ your-app-name/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Switch} from 'react-router-dom';
 import Home from './Home';
 import AddProducts from './AddProducts';
 function App()
@@ -10,11 +9,11 @@ function App()
         <h1>
           my app
         </h1>
-        <Switch>
+      
           <Route exact path = "/" component = {Home}/>
           <Route path = "/add-product"
           component = {AddProducts}/>
-        </Switch>
+    
       </div>
     </Router>
   );
